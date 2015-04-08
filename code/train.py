fdf56cda2eff0a178706199af201943b591e049c
Jason Parham <bluemellophone@gmail.com>
2015-04-08 12:29:29 -0400

    wip

--- code/train.py
+++ code/train.py
@@ -42,14 +42,14 @@ def batch_iterator(X, y, bs, norm=None, mean=None, std=None, augment=None):
         yb_ = yb.astype(np.int32)
         # Whiten
         if mean is not None:
-            Xb -= mean
+            Xb_ -= mean
         if std is not None:
-            Xb /= std
+            Xb_ /= std
         if norm is not None and norm > 0.0:
             Xb_ /= norm
         # Augment
         if augment is not None:
-            Xb, yb = augment(Xb, yb)
+            Xb_, yb_ = augment(Xb_, yb_)
         yield Xb_, yb_
 
 
