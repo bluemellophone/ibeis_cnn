c7b5efea818533704010abe0b3bc83f6213c3fc4
Jason Parham <bluemellophone@gmail.com>
2015-04-22 17:11:26 -0400

    Ordered labels

--- ibeis_cnn/utils.py
+++ ibeis_cnn/utils.py
@@ -552,7 +552,7 @@ def show_confusion_matrix(correct_y, predict_y, category_list, results_path,
         print(values)
         assert len(list(set(values))) == len(values), 'Mapped categories have a duplicate assignment'
         assert 0 in values, 'Mapped categories must have a 0 index'
-        temp = list(category_list.iteritems())
+        temp = list(category_mapping.iteritems())
         temp = sorted(temp, key=itemgetter(1))
         category_list_ = [ t[1] for t in temp ]
 
