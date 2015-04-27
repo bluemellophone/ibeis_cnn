599f614e20e0de23f5edcc812792b8f40c3307e4
Jason Parham <bluemellophone@gmail.com>
2015-04-27 14:13:53 -0400

    iter funcs update

--- ibeis_cnn/utils.py
+++ ibeis_cnn/utils.py
@@ -369,7 +369,8 @@ def batch_iterator(X, y, batch_size, encoder=None, rand=False, augment=None,
             yb = encoder.transform(yb)
         # Get corret dtype for y (after encoding)
         if yb is not None:
-            yb = np.vstack((yb, -np.ones(len(yb)))).T.flatten()
+            # TODO: FIX data_per_label ISSUES
+            # yb = np.vstack((yb, -np.ones(len(yb)))).T.flatten()
             yb = yb.astype(np.int32)
         # Convert cv2 format to Lasagne format for batching
         Xb = Xb.transpose((0, 3, 1, 2))
