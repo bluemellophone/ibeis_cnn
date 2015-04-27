f764a853abe9f0d0f83bf6157ad9a3ec07dd9617
Jason Parham <bluemellophone@gmail.com>
2015-04-27 11:58:11 -0400

    Caffenet displayer

--- ibeis_cnn/utils.py
+++ ibeis_cnn/utils.py
@@ -699,6 +699,7 @@ def show_convolutional_features(weights_list, results_path, color=False, limit=1
         for f, feature in enumerate(all_weights):
             # get all the weights and scale them to dimensions that can be shown
             if color:
+                feature = feature[::-1]  # Rotate BGR to RGB
                 feature = cv2.merge(feature)
             fmin, fmax = np.min(feature), np.max(feature)
             print('0', feature.shape, fmin, fmax)
@@ -706,9 +707,10 @@ def show_convolutional_features(weights_list, results_path, color=False, limit=1
             feature = (feature - fmin) * (255. / domain)
             fmin, fmax = np.min(feature), np.max(feature)
             print('1', feature.shape, fmin, fmax)
+            feature = feature.astype(np.int32)
             if color:
-                cv2.imshow('', feature)
-                cv2.waitKey(0)
+                # cv2.imshow('', feature)
+                # cv2.waitKey(0)
                 grid[f].imshow(feature, interpolation='nearest')
             else:
                 grid[f].imshow(feature, cmap=cm.Greys_r, interpolation='nearest')
