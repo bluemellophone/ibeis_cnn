1a2af4263f02cd1fff94ebf9d13c9ec443ed9e4e
joncrall <erotemic@gmail.com>
2015-12-01 13:49:31 -0500

    wip

--- ibeis_cnn/netrun.py
+++ ibeis_cnn/netrun.py
@@ -87,6 +87,7 @@ def netrun():
         python -m ibeis_cnn --tf netrun --db PZ_Master1 --acfg default:is_known=True,qmin_pername=2,view=primary,species=primary,minqual=ok --ensuredata --show --vtd
         python -m ibeis_cnn --tf netrun --ds pzm4 --weights=new --arch=siaml2_128 --train --monitor
         python -m ibeis_cnn --tf netrun --ds pzm4 --arch=siaml2_128 --test
+        python -m ibeis_cnn --tf netrun --ds pzm4 --arch=siaml2_128 --veryverbose --no-flask
 
         # --- TRAINING ---
         python -m ibeis_cnn --tf netrun --db PZ_Master1 --acfg default:is_known=True,qmin_pername=2,view=primary,species=primary,minqual=ok --weights=new --arch=siaml2_128 --train --monitor
