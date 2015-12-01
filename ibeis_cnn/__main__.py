e7e034aa7e1e1ff5f0dc666fe313a2001b5e3851
joncrall <erotemic@gmail.com>
2015-12-01 11:51:54 -0500

    wip

--- ibeis_cnn/__main__.py
+++ ibeis_cnn/__main__.py
@@ -8,9 +8,13 @@ def ibeis_cnn_main():
     ignore_suffix = []
     import utool as ut
     try:
+        print('[ibeis_cnn] Importing ibeis_cnn')
         import ibeis_cnn  # NOQA
+        print('[ibeis_cnn] Importing ibeis')
         import ibeis  # NOQA
+        print('[ibeis_cnn] Importing ibeis_cnn._plugin')
         import ibeis_cnn._plugin  # NOQA
+        print('[ibeis_cnn] Done importing for __main__')
     except ImportError:
         raise
         pass
